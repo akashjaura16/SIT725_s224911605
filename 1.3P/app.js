36148ab476618c0da6c20ad7999bb4fa7c2495f5
Akashdeep Singh <akashjaura016@gmail.com>
2025-11-10 18:52:02 +1100

    Added SIT725 Task 1.3P JavaScript file

---
+++ 1.3P/app.js
@@ -0,0 +1,4 @@
+// app.js
+console.log("Welcome to SIT725 Practical 1 - Basics of Programming and Git");
+let name = "Akashdeep Singh";
+console.log("Hello " + name + "! Let's start learning JavaScript and Git.");
